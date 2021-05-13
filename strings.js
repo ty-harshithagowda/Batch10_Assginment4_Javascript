30f1c2a3a668fc823317ade29b61ff2092d00fe4
ty-harshithagowda <harshitha.br@testyantra.in>
2021-05-13 21:02:18 +0530

    added assignments

---
+++ strings.js
@@ -0,0 +1,25 @@
+const sentence = 'Hi, i am developer';
+const str2='what are u doing'
+console.log("toLowerCase() method");
+console.log(sentence.toLowerCase());
+
+console.log("toUpperCase() method");
+console.log(sentence.toUpperCase());
+
+console.log("charAt() method");
+console.log(sentence.charAt(5));
+
+console.log("indexOf() method");
+console.log(sentence.indexOf('hrx'));
+
+console.log("concat() method");
+console.log(sentence.concat(' ',str2));
+
+console.log("includes() method");
+console.log(sentence.includes('Hrx'));
+
+console.log("replace() method");
+console.log(sentence.replace('hrx','rahul'));
+
+console.log("substring() method");
+console.log(sentence.substr(4,3));
